30c4fe3d2e8961ca1b75f6dfaf78677488d63cb2
Dante Cerron <dacerron@gmail.com>
2018-06-12 14:37:02 -0700

    everything is served correctly

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req,res) => {
-        res.sendFile(__dirname + "/holobrain-webvr/index.html")
+        res.sendFile(__dirname + "/index.html")
 });
+
+app.use("/public/assets", express.static(__dirname + "/public/assets"))
+app.use("/js", express.static(__dirname + "/js"))
 
 app.listen(port)
