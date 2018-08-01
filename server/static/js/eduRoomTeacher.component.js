47403f7b04bf796d800f51333c51b3f4d77fe134
dacerron <dacerron@gmail.com>
2018-08-01 13:43:30 -0700

    typo

--- server/static/js/eduRoomTeacher.component.js
+++ server/static/js/eduRoomTeacher.component.js
@@ -307,7 +307,7 @@ AFRAME.registerComponent('eduroomteacher', {
                     stream.write(left);
                 }
                 
-                var ss = require("js/socket.io-strea.js");
+                var ss = require("js/socket.io-stream.js");
                 stream = ss.createStream();
                 var socket = io(window.location.origin + '/' + this.sessionKey);
                 ss(socket).emit('audio', stream)
