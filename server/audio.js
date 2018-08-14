7eb5c15556a1c0d44fbcab5c31a87c5a40820a3c
dacerron <dacerron@gmail.com>
2018-08-14 14:19:13 -0700

    teacherSocket

--- server/audio.js
+++ server/audio.js
@@ -8,16 +8,21 @@ var Audio = (function() {
             allowHalfOpen: true
         });
         var first = false;
+        var teacherSocket;
         var audio = io
         .of('/' + sessionKey)
         .on('connection', function(socket) {
             ss(socket).on('audio', function(incomingstream) {
+                teacherSocket = socket;
                 incomingstream.pipe(interStream);
                 console.log("piped incoming audio");
             });
             ss(socket).on('join', function(stream) {
                 interStream.pipe(stream);
-                socket.emit('ready');
+                if(!first) {
+                    teacherSocket.emit('ready');
+                    first = true;
+                }
             });
         });
         console.log("audio stream ready");
