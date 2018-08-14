a1cc1e007d2caf77dc02ef2749ebbde67b9d3aec
dacerron <dacerron@gmail.com>
2018-08-14 15:00:42 -0700

    no rest of network

--- server/static/js/eduRoomStudent.component.js
+++ server/static/js/eduRoomStudent.component.js
@@ -180,6 +180,6 @@ AFRAME.registerComponent("eduroomstudent", {
         initializePlayer(stream);
         var socket = io(window.location.origin + '/' + this.sessionKey);
         ss(socket).emit('join', stream);
-        this.share();
+        //this.share();
     }
 });
