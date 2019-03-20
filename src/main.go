dc39cd14372013e9853c53513b2e643ffb78d825
dacerron <dacerron@gmail.com>
2019-03-20 12:35:18 -0700

    solo mode

--- src/main.go
+++ src/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"./comm"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"./comm"
 )
 
 func main() {
@@ -19,30 +20,30 @@ func main() {
 	})
 
 	http.HandleFunc("/teacher/shareHighlight", func(w http.ResponseWriter, r *http.Request) {
-			switch r.Method {
-			case "POST":
-				r.ParseForm()
-				buf := make([]byte, 4096)
-				r.Body.Read(buf)
-				//fmt.Println("calling ReceiveHighlight with", string(buf))
-				server.ReceiveHighlight(r.Form["id"][0], buf)
-				w.Write([]byte("ack"))
-			default:
-				http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-			}
+		switch r.Method {
+		case "POST":
+			r.ParseForm()
+			buf := make([]byte, 4096)
+			r.Body.Read(buf)
+			//fmt.Println("calling ReceiveHighlight with", string(buf))
+			server.ReceiveHighlight(r.Form["id"][0], buf)
+			w.Write([]byte("ack"))
+		default:
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
 	})
 
 	http.HandleFunc("/teacher/shareInfo", func(w http.ResponseWriter, r *http.Request) {
-			switch r.Method {
-			case "POST":
-				r.ParseForm()
-				buf := make([]byte, 4096)
-				r.Body.Read(buf)
-				server.ReceiveInfo(r.Form["id"][0], buf)
-				w.Write([]byte("ack"))
-			default:
-				http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-			}
+		switch r.Method {
+		case "POST":
+			r.ParseForm()
+			buf := make([]byte, 4096)
+			r.Body.Read(buf)
+			server.ReceiveInfo(r.Form["id"][0], buf)
+			w.Write([]byte("ack"))
+		default:
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
 	})
 
 	http.HandleFunc("/student/requestInfo", func(w http.ResponseWriter, r *http.Request) {
@@ -73,8 +74,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	http.Handle("/", http.FileServer(http.Dir(dir + "\\public\\")))
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	http.Handle("/", http.FileServer(http.Dir(dir+"\\public\\")))
+	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
