77f44eaa0c315a32dfc082ba312be01fe2e39705
Kimberly Burke <kburke@sssmri.com>
2019-09-30 12:00:33 -0700

    Update with low-res models
    Updating glitch version with the low-res model locations

--- src/public/js/structure-manager.js
+++ src/public/js/structure-manager.js
@@ -90,26 +90,26 @@ StructureManager = (function () {
     }
 
     var src = {
-        cortex: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2Fcortex.glb?v=1569018937514",
-        arteries: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2Farteries.glb?v=1569023917900",
-        cerebellum: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2Fcerebellum.gltf?v=1568749643179",
-        fornix: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2Ffornix.glb?v=1569018963022",
-        hippocampus: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2Fhippocampus.glb?v=1569018995173",
-        lamygdala: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FL-Amygdala.gltf?v=1568749647133",
-        lcaudate: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FL-caudate.glb?v=1569019022423",
-        lglobus: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FL-Globus.gltf?v=1568749646667",
-        lputamen: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FL-Putamen.gltf?v=1568749646531",
-        mamtract: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FMamilloThalamicTract.gltf?v=1568749646684",
-        mambodies: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FMammillaryBodies.gltf?v=1568749645589",
-        ramygdala: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FR-Amygdala.gltf?v=1568749645259",
-        rcaudate: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FR-caudate.glb?v=1569019044324",
-        rglobus: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FR-Globus.gltf?v=1568749644698",
-        rputamen: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FR-Putamen.gltf?v=1568749646382x",
-        sinuses: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2Fsinus.glb?v=1569019066725",
-        subnigra: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FSubstantia-Nigra.gltf?v=1568749643495",
-        subthalamic: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2FSubthalamic-Nuclei.gltf?v=1568749643194",
-        thalamus: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2Fthalamus.glb?v=1569019088308",
-        ventricles: "https://cdn.glitch.com/4d11bd5a-9e2e-4e8c-a373-d57ffc64d547%2Fventricles.glb?v=1569019102223"
+        cortex: "../assets/cortex.glb",
+        arteries: "../assets/arteries.glb",
+        cerebellum: "../assets/cerebellum.gltf",
+        fornix: "../assets/fornix.glb",
+        hippocampus: "../assets/hippocampus.glb",
+        lamygdala: "../assets/L-Amygdala.gltf",
+        lcaudate: "../assets/L-caudate.glb",
+        lglobus: "../assets/L-Globus.gltf",
+        lputamen: "../assets/L-Putamen.gltf",
+        mamtract: "../assets/MamilloThalamicTract.gltf",
+        mambodies: "../assets/MammillaryBodies.gltf",
+        ramygdala: "../assets/R-Amygdala.gltf",
+        rcaudate: "../assets/R-caudate.glb",
+        rglobus: "../assets/R-Globus.gltf",
+        rputamen: "../assets/R-Putamen.gltf",
+        sinuses: "../assets/sinus.glb",
+        subnigra: "../assets/Substantia-Nigra.gltf",
+        subthalamic: "../assets/Subthalamic-Nuclei.gltf",
+        thalamus: "../assets/thalamus.glb",
+        ventricles: "../assets/ventricles.glb"
     }
     var rules = {
         cortex: {
